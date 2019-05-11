fca88bf4acf7d0cbbbe1b28dbe5df1fbc6b88a83
卫龙 <ppfire@tg10010.com>
2019-05-11 10:35:33 +0800

    增加websocket

---
+++ api/app/Http/Controllers/Merchant/AuthController.php
@@ -0,0 +1,89 @@
+<?php
+namespace App\Http\Controllers\Merchant;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:merchant', ['except' => ['login']]);
+    }
+
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+
+        $this->validate($request, [
+            'username' => 'required', 'password' => 'required',
+        ]);
+
+        $credentials = $request->only('username', 'password');
+        if (!$token = JWTAuth::attempt($credentials)) {
+            return $this->error(10002);
+        }
+
+        return $this->respondWithToken($token);
+
+    }
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json(JWTAuth::parseToken()->touser());
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        JWTAuth::parseToken()->invalidate();
+
+        return $this->success();
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(JWTAuth::parseToken()->refresh());
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return $this->success([
+            'accessToken' => $token,
+            'tokenType' => 'bearer',
+            'expiresIn' => JWTAuth::factory()->getTTL() * 60,
+        ]);
+    }
+}
---
+++ api/routes/merchant.php
@@ -0,0 +1,9 @@
+<?php
+
+$router->group(['prefix' => 'merchant', 'namespace' => 'Merchant'], function () use ($router) {
+    $router->group(['prefix' => 'auth'], function () use ($router) {
+        $router->post('login', 'AuthController@login');
+        $router->post('logout', 'AuthController@logout');
+        $router->post('refresh', 'AuthController@refresh');
+    });
+});
